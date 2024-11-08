1de9fb3b060bb34d6bb4ec82703d35949746a974
xuxiaoxuxiao <xiaoxu2016@gmail.com>
2024-11-07 23:25:57 -0800

    3.5 finished

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
+import cors from "cors";
 const app = express()
+app.use(cors());
 Lab5(app)
 Hello(app)
 
