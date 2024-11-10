b08278756ef0e84efb8cec3c6aa7d4ab398095e0
xuxiaoxuxiao <xiaoxu2016@gmail.com>
2024-11-09 21:49:01 -0800

    4.2 finished

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from 'express'
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
+import UserRoutes from "./Kanbas/Users/routes.js";
+
 const app = express()
 app.use(cors());
 app.use(express.json())
 Lab5(app)
 Hello(app)
-
+UserRoutes(app);
 app.listen(process.env.PORT || 4000)
