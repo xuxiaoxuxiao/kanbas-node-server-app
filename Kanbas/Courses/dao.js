2a3c5526137cc4c75a06c2c226a82ffe474c5b30
xuxiaoxuxiao <xiaoxu2016@gmail.com>
2024-11-30 22:40:07 -0800

    temp commit

--- Kanbas/Courses/dao.js
+++ Kanbas/Courses/dao.js
@@ -1,14 +1,19 @@
-import Database from "../Database/index.js";
+import model from "./model.js";
+
+// export function deleteCourse(courseId) {
+//   const { courses, enrollments } = Database;
+//   Database.courses = courses.filter((course) => course._id !== courseId);
+//   Database.enrollments = enrollments.filter(
+//     (enrollment) => enrollment.course !== courseId
+// );}
 
 export function deleteCourse(courseId) {
-  const { courses, enrollments } = Database;
-  Database.courses = courses.filter((course) => course._id !== courseId);
-  Database.enrollments = enrollments.filter(
-    (enrollment) => enrollment.course !== courseId
-);}
+  return model.deleteOne({ _id: courseId });
+ }
+ 
 
 export function findAllCourses() {
-  return Database.courses;
+  return model.find();
 }
 export function findCoursesForEnrolledUser(userId) {
     const { courses, enrollments } = Database;
@@ -16,15 +21,20 @@ export function findCoursesForEnrolledUser(userId) {
       enrollments.some((enrollment) => enrollment.user === userId && enrollment.course === course._id));
     return enrolledCourses;
   }
+
   export function createCourse(course) {
-    const newCourse = { ...course, _id: Date.now().toString() };
-    Database.courses = [...Database.courses, newCourse];
-    return newCourse;
-  }
+    delete course._id;
+    return model.create(course);
+    // const newCourse = { ...course, _id: Date.now().toString() };
+    // Database.courses = [...Database.courses, newCourse];
+    // return newCourse;
+   }
+   
   export function updateCourse(courseId, courseUpdates) {
-    const { courses } = Database;
-    const course = courses.find((course) => course._id === courseId);
-    Object.assign(course, courseUpdates);
-    return course;
+    return model.updateOne({ _id: courseId }, { $set: courseUpdates });
+    // const { courses } = Database;
+    // const course = courses.find((course) => course._id === courseId);
+    // Object.assign(course, courseUpdates);
+    // return course;
   }
   
--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -2,24 +2,35 @@ import * as dao from "./dao.js";
 import * as modulesDao from "../Modules/dao.js";
 import * as assignmentsDao from "../Assignments/dao.js";
 export default function CourseRoutes(app) {
-  app.get("/api/courses", (req, res) => {
-    const courses = dao.findAllCourses();
-    res.send(courses);
+  app.post("/api/courses", async (req, res) => {
+    const course = await dao.createCourse(req.body);
+    res.json(course); 
   });
 
-  app.delete("/api/courses/:courseId", (req, res) => {
+  // app.delete("/api/courses/:courseId", (req, res) => {
+  //   const { courseId } = req.params;
+  //   dao.deleteCourse(courseId);
+  //   res.sendStatus(204);
+  // });
+  app.delete("/api/courses/:courseId", async (req, res) => {
     const { courseId } = req.params;
-    dao.deleteCourse(courseId);
-    res.sendStatus(204);
+    const status = await dao.deleteCourse(courseId);
+    res.send(status);
   });
-
-  app.put("/api/courses/:courseId", (req, res) => {
+ 
+  // app.put("/api/courses/:courseId", (req, res) => {
+  //   const { courseId } = req.params;
+  //   const courseUpdates = req.body;
+  //   dao.updateCourse(courseId, courseUpdates);
+  //   res.sendStatus(204);
+  // });
+  app.put("/api/courses/:courseId", async (req, res) => {
     const { courseId } = req.params;
     const courseUpdates = req.body;
-    dao.updateCourse(courseId, courseUpdates);
-    res.sendStatus(204);
+    const status = await dao.updateCourse(courseId, courseUpdates);
+    res.send(status);
   });
-  
+ 
   ///Module    
   app.get("/api/courses/:courseId/modules", (req, res) => {
     const { courseId } = req.params;
